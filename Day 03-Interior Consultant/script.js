572f77aea89fffa4afedf87345f282c337dc34ca
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-02-06 10:38:11 +0200

    upload day-3

---
+++ Day 03-Interior Consultant/script.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".menu");
+let header = document.querySelector(".container-nav");
+menu.addEventListener("click",function(){
+    header.classList.toggle("nav-open")
+})
